db54d03fefba7f04030e77c3ad4366ec5f2f03d8
Priscila <priscila.campoos@yahoo.com.br>
2024-03-30 15:26:55 +0000

    created collection module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: "", redirectTo: "homepage", pathMatch: "full"},
-  { path: 'homepage', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) }
+  { path: 'homepage', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) },
+  { path: 'collection', loadChildren: () => import('./collection/collection.module').then(m => m.CollectionModule) }
 
 
 ];
